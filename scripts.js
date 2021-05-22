7af7a55cc0f1270ab51de8d95566e560bf9e9603
billowbashir <billowbashir@gmail.com>
2021-05-22 15:22:47 +0300

    add comments

--- scripts.js
+++ scripts.js
@@ -20,26 +20,34 @@ const messageElement = document.getElementById('message');
 const typedValueElement = document.getElementById('typed-value');
 
 
-document.getElementById('start').addEventListener('click',()=>{
-    const quoteIndex=Math.floor(Math.random()*quotes.length);
-    const quote=quotes[quoteIndex];
-
-    words=quote.split(' ');
-
-    wordIndex=0;
-
-    const spanWords=words.map(function(word){return "<span>${word}</span>"});
-
+// at the end of script.js
+document.getElementById('start').addEventListener('click', () => {
+    // get a quote
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[quoteIndex];
+    // Put the quote into an array of words
+    words = quote.split(' ');
+    // reset the word index for tracking
+    wordIndex = 0;
+  
+    // UI updates
+    // Create an array of span elements so we can set a class
+    const spanWords = words.map(function(word) { return `<span>${word} </span>`});
+    // Convert into string and set as innerHTML on quote display
     quoteElement.innerHTML = spanWords.join('');
-
+    // Highlight the first word
     quoteElement.childNodes[0].className = 'highlight';
-
+    // Clear any prior messages
     messageElement.innerText = '';
-
+  
+    // Setup the textbox
+    // Clear the textbox
     typedValueElement.value = '';
-
+    // set focus
     typedValueElement.focus();
-
+    // set the event handler
+  
+    // Start the timer
     startTime = new Date().getTime();
-
-});
+  });
+  
